0db36ee9c4d4eb4b7daeb3fce5f249cf5d859580
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-10-21 12:43:20 +0500

    alb-ver-updated

--- terraform-menifest/c10-02-ALB-application-loadbalancer.tf
+++ terraform-menifest/c10-02-ALB-application-loadbalancer.tf
@@ -1,7 +1,8 @@
 # Terraform AWS Application Load Balancer (ALB)
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "5.16.0"
+  # version = "5.16.0"
+  version = "6.0.0"
 
   name               = "${local.name}-alb"
   load_balancer_type = "application"
