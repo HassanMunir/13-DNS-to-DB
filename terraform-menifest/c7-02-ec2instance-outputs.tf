c3e7c0af86a57cd7fd745853d95e09c19e410557
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-10-24 15:13:56 +0500

    ec2-out-modfd

--- terraform-menifest/c7-02-ec2instance-outputs.tf
+++ terraform-menifest/c7-02-ec2instance-outputs.tf
@@ -37,3 +37,14 @@ output "app2_ec2_private_ip" {
   value       = module.ec2_private_app2.private_ip
 }
 
+# App3 - Private EC2 Instances
+## ec2_private_instance_ids
+output "app3_ec2_private_instance_ids" {
+  description = "List of IDs of instances"
+  value       = module.ec2_private_app3.id
+}
+## ec2_private_ip
+output "app3_ec2_private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  value       = module.ec2_private_app3.private_ip
+}
